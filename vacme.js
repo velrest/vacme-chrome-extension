711915302e5ce10abb09651537f36d1f156b6e43
Jonas Cosandey <jonas.cosandey@adfinis.com>
2021-06-10 12:20:51 +0200

    feat(pause): add link to pause notification so you can apply for a slot

--- vacme.js
+++ vacme.js
@@ -1,8 +1,40 @@
+let pause = false;
+
+async function initButton() {
+  let select = document.querySelector("select");
+  if (!select) {
+    return setTimeout(initButton, 100);
+  }
+  const a = document.createElement("a");
+  a.innerText = "Pause notifications";
+  a.addEventListener("click", function () {
+    pause = !pause;
+    a.innerText = pause ? "Resume notifications" : "Pause notifications";
+    let notification;
+    if (!pause) {
+      checkVacme();
+      notification = new Notification("Vacme", {
+        body: "Notifications resumed.",
+      });
+    } else {
+      notification = new Notification("Vacme", {
+        body: "Notifications paused.",
+      });
+    }
+    setTimeout(() => {
+      notification.close();
+    }, 1000);
+  });
+  select.parentNode.insertBefore(a, select);
+}
+
 let checkVacme = async function () {
-  location.reload();
-  let available = Array.from(
-    document.querySelectorAll("select .ng-star-inserted")
-  )
+  let list = Array.from(document.querySelectorAll("select .ng-star-inserted"));
+  if (!list.length && list.length < 2) {
+    console.info("Timeout: List not rendered");
+    return setTimeout(checkVacme, 100);
+  }
+  let available = list
     .slice(2)
     .map((e) => e.innerText)
     .filter((t) => !t.includes("keine Termine"));
@@ -12,11 +44,19 @@ let checkVacme = async function () {
     const notification = new Notification("Vacme", {
       body: "Available: " + available.join(", "),
     });
+    setTimeout(() => {
+      notification.close();
+    }, 2000);
   } else {
     console.info({ permission, available });
   }
 
-  setTimeout(checkVacme, 10000);
+  setTimeout(() => {
+    if (!pause) {
+      location.reload();
+    }
+  }, 10000);
 };
 
-setTimeout(checkVacme, 10000);
+initButton();
+checkVacme();
